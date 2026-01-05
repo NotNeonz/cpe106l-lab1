26aca926a36e8254349893a522d2705b63510a4c
NotNeonz <99581880+NotNeonz@users.noreply.github.com>
2026-01-05 18:34:38 +0800

    Update lab2_student_record.py

--- lab2_student_record.py
+++ lab2_student_record.py
@@ -6,3 +6,10 @@ student={
 
 average=sum(student["grades"])/len(student["grades"])
 print("Average:", average)
+
+# Add grades
+student["grades"].append(75)
+print("Updated Grades:", grades)
+
+# Update student information
+student["name"] =
