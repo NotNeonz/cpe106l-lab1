4ebfa6f4df48ef50ae08f68cc26752eedaa53df6
NotNeonz <99581880+NotNeonz@users.noreply.github.com>
2026-01-05 18:25:03 +0800

    Update Student Record System.py

--- Student Record System.py
+++ Student Record System.py
@@ -2,7 +2,7 @@ student={
   "id":"2025-001",
   "name":"Juan Dela Cruz",
   "grades":[88,90,85]
-]
+}
 
-average=sum(student["grades"]/len(student["grades"])
+average=sum(student["grades"])/len(student["grades"])
 print("Average:", average)
