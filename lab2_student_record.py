f85fdbc8572419b57044c5df43a78b10b559d053
NotNeonz <99581880+NotNeonz@users.noreply.github.com>
2026-01-06 09:17:33 +0800

    Update lab2_student_record.py

--- lab2_student_record.py
+++ lab2_student_record.py
@@ -10,21 +10,35 @@ average=sum(student["grades"])/len(student["grades"])
 # Show current student ID, updated grades & average
 print("ID:", student["id"])
 print("Student Name:", student["name"])
-print("Updated Grades:", student["grades"])
+print("Grades:", student["grades"])
 print("Average:", average)
 
 # Blank prints
 print()
 
-# Add grade, and new average
-ask_id=input("Enter student ID to update: ")
+# Choice for user to (1)add grade, (2)update student info, or (3)terminate program
+print("(1) Add student grade")
+print("(2) Update student information")
+print("(Any Number) Terminate program")
 
-if ask_id==student["id"]:
-    updated_grade=int(input("Enter new grade for student: "))
-    student["grades"].append(updated_grade)
-    print("Updated Grade:",student["grades"])
-    average=sum(student["grades"])/len(student["grades"])
-    print("Updated average:",average)
+choice=input("Select option from 1-2 or any number: ")
+
+# Add grade, and new average
+if choice == "1":
+    print()
+    ask_id=input("Enter student ID to update: ")
+    if ask_id==student["id"]:
+        updated_grade=int(input("Enter new grade for student: "))
+        student["grades"].append(updated_grade)
+        print()
+        print("Updated grades:",student["grades"])
+        average=sum(student["grades"])/len(student["grades"])
+        print("Updated average:",average)
     
+    else:
+        print()
+        print("No student was found with the input ID")
+
 else:
-    print("No student was found with the input ID")
+    print()
+    print("Program Terminated... Goodbye! - Ignacio, Juan Carlos Miguel")
